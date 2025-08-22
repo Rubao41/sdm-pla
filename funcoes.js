1bd0d85a070cef7284112d40a324428989a73080
Arthur Felix de Avila <824148538@ulife.com.br>
2025-08-22 11:49:05 -0300

    Conhcendo funções em js

---
+++ funcoes.js
@@ -0,0 +1,21 @@
+// função declarativa
+function saudacao(nome){
+    return `Ola, ${nome}!`
+}
+console.log(saudacao('Arthur'))
+
+// função anônima
+const soma = function(a, b) {
+    return a + b
+}
+console.log(soma(7, 7))
+
+// função simples (uma bosta!)
+
+function simp(){
+    let a = 7
+    let b = -7
+    let c = a + b
+    console.log('O valor de c é : ' + c)
+}
+simp()  
